78ce576140aef811b1cb25183e4d5f0ec382baac
tuan <tuan-vscode@tanhocnn>
2022-08-31 04:32:10 +0700

    Bai 3.5

--- Models/Book.cs
+++ Models/Book.cs
@@ -2,18 +2,66 @@ namespace Bookman.ConsoleApp.Models
 {
     class Book
     {
-        int _id;
-        string _authors;
-        string _title;
-        string _publisher;
-        int _year;
-        int _eddition;
-        string _isbn;
-        string _tags;
-        string _description;
-        int _rating;
-        bool _reading;
-        string _file;
+        private int _id = 1;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <value></value>
+        public int Id
+        { 
+            get => _id; 
+            set { if (value >= 1) _id = value; }
+        }
+        private string _authors = "Unknow author";
+        public string Authors
+        {
+            get { return _authors; }
+            set { if (!string.IsNullOrEmpty(value)) _authors = value; }
+        }
+        private string _title = "A new book";
+        public string Title
+        {
+            get { return _title; }
+            set { if (!string.IsNullOrEmpty(value)) _title = value; }
+        }
+        private string _publisher = "Unknown publisher";
+        public string Publisher
+        {
+            get { return _publisher; }
+            set { if (!string.IsNullOrEmpty(value)) _publisher = value; }
+        }
+        private int _year = 2018;
+        public int Year
+        {
+            get { return _year; }
+            set { if (value>=1950) _year = value; }
+        }
+        private int _edition = 1;
+        public int Edition
+        {
+            get { return _edition; }
+            set { if (value >= 1) _edition =value; }
+        }
+        public string Isbn { get; set; } = "";
+        public string Tags { get; set; } = "";
+        public string Description { get; set; } = "A new book";
+        private int _rating = 1;
+        public int Rating
+        {
+            get { return _rating; }
+            set { if (value>=1 && value<=5) _rating =value; }
+        }
+        public bool Reading { get; set; }
+        private string _file;
+        public string File
+        {
+            get { return _file; }
+            set { if (System.IO.File.Exists(value)) _file =value; }
+        }
+        public string FileName
+        {
+            get { return System.IO.Path.GetFileName(_file); }
+        }
 
     }
 }
