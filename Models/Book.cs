76a39944dc2b0e815b0d8821cd7b3bf0d5df9002
tuan <tuan-vscode@tanhocnn>
2022-08-30 22:33:24 +0700

    Bai 3.1

---
+++ Models/Book.cs
@@ -0,0 +1,19 @@
+namespace Bookman.ConsoleApp.Models
+{
+    class Book
+    {
+        int _id;
+        string _authors;
+        string _title;
+        string _publisher;
+        int _year;
+        int _eddition;
+        string _isbn;
+        string _tags;
+        string _description;
+        int _rating;
+        bool _reading;
+        string _file;
+
+    }
+}
